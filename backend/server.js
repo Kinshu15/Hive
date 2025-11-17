c965d8d3ccfa37aed24b2bb27395d8ee0f285587
Kushagra Bhardwaj <kushagrabhardwaj@Kinshus-Macbook.local>
2025-11-17 17:32:01 +0530

    depl

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000","https://hive-blow.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
