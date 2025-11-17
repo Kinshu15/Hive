5f51ca20c011789ee133e3f86f27f616939fef3b
Kushagra Bhardwaj <kushagrabhardwaj@Kinshus-Macbook.local>
2025-11-17 17:42:31 +0530

    asdFDGH

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://hive-blow.onrender.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 connectDB();
 
